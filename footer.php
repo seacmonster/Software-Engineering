ca95748f5b99dda8dcbbd65cdb945ee7bb72109c
FrogRider <cbondy18@my.whitworth.edu>
2018-04-19 11:57:20 -0700

    Home page redesign
    Redesigned homepage

---
+++ footer.php
@@ -0,0 +1,31 @@
+<!-- Contact Section -->
+<footer class="footer">
+      <div class="container">
+      <span class="text-muted">
+        <div class="row">
+          <div class="col-md-6 mx-auto text-center">
+            <h2 class="section-heading">Connect With Us!</h2>
+            <hr class="primary">
+            <p>Check us out on social media</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-4 ml-auto text-center">
+            <a class="fa fa-twitter fa-3x sr-contact" href="https://twitter.com/" style="text-decoration:none"></a>
+            <br>
+            <a href="https://twitter.com/">Twitter</a>
+          </div>
+          <div class="col-lg-4 ml-auto text-center">
+            <a class="fa fa-instagram fa-3x sr-contact" href="https://www.instagram.com/whitworthfm/" style="text-decoration:none"></a>
+            <br>
+            <a href="https://www.instagram.com/">Instagram</a>
+          </div>
+          <div class="col-lg-4 ml-auto text-center">
+            <a class="fa fa-facebook" href="https://www.facebook.com/" style="text-decoration:none"></a>
+            <br>
+            <a href="https://www.facebook.com/">facebook</a>
+          </div>
+        </div>
+        </span>
+      </div>
+    </footer>
---
+++ nav.php
@@ -0,0 +1,34 @@
+<nav class="navbar fixed-top navbar-expand-md navbar-dark bg-secondary">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                  <ul class="navbar-nav">
+                    <li class="nav-item active">
+                      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Lab Members</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">News</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Publications</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Grants</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Research</a> <!--Dropdown menu with worms and zebrafish-->
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Database</a>
+                    </li>
+                  </ul>
+                </div>
+              </nav>
